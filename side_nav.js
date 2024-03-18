d2067216df68a5308372f704f89b1945a1576798
Harshal-Maru-2303113 <159224269+Harshal-Maru-2303113@users.noreply.github.com>
2024-03-18 11:45:37 +0530

    Add files via upload

---
+++ side_nav.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("side_nav").style.display = "block";
+    document.getElementById("side_nav").style.width = "200px";
+    document.getElementById("symbol_nav").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("side_nav").style.width = "0";
+    setTimeout(delay,450);
+    
+}
+function delay(){
+    document.getElementById("symbol_nav").style.display = "block"
+}
